078c053c8faed6044b1c76fd9616ad7b7b74e10e
zecozejak <alexcros2002@gmail.com>
2023-12-21 15:47:05 +0100

    First commit
    Simple files (server, client)

---
+++ src/client/client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mac2019 on 21/12/2023.
+//
+
+#include "client.h"
---
+++ src/client/client.h
@@ -0,0 +1,14 @@
+//
+// Created by Mac2019 on 21/12/2023.
+//
+
+#ifndef PAINTTHEMOST_GAME_CLIENT_H
+#define PAINTTHEMOST_GAME_CLIENT_H
+
+
+class client {
+
+};
+
+
+#endif //PAINTTHEMOST_GAME_CLIENT_H
