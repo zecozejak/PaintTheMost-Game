280d8054a35c7afc053cf99a69f534053f97957b
juliamanikowska <135166770+juliamanikowska@users.noreply.github.com>
2024-01-10 19:11:15 +0100

    Update game-test.cpp

--- game/game-test.cpp
+++ game/game-test.cpp
@@ -11,6 +11,7 @@ const int windowWidth = 800;
 const int windowHeight = 600;
 const float squareSize = 10.0f;  //rozmiar kwadracika/ikonki
 const float outlineThickness = 1.0f;  //grubosc obramowania kwardacika
+const sf::Time roundTime = sf::seconds(90.0f); //czas gry
 
 int main() {
     //okno wyskakujace
@@ -59,13 +60,9 @@ int main() {
     //siatka planszy
     std::vector<std::vector<sf::Color>> visited(gridWidth / gridSize, std::vector<sf::Color>(gridHeight / gridSize, sf::Color::White));
 
-    //slad
-    //sf::RectangleShape trail(sf::Vector2f(gridSize, gridSize));
-    //trail.setFillColor(sf::Color::Red);  
-
     //licznik czasu
     sf::Clock clock;
-    sf::Time initialTime = sf::seconds(90);
+    bool timeExpired = false;
 
     //main petla
     while (window.isOpen()) {
@@ -78,99 +75,105 @@ int main() {
 
         //obliczanie pozostalego czasu
         sf::Time elapsedTime = clock.getElapsedTime();
-        sf::Time remainingTime = initialTime - elapsedTime;
+        sf::Time remainingTime = roundTime - elapsedTime;
 
+        if (remainingTime <= sf::Time::Zero) {
+            timeExpired = true;
+            remainingTime = sf::Time::Zero;  //unikanie wartosci ujemnych
+        }
 
         //wyswietlanie licznika w terminalu
         std::cout << "\rPozostaly czas: " << std::setfill('0') << std::setw(2) << static_cast<int>(remainingTime.asSeconds()) / 60 << ":"
             << std::setfill('0') << std::setw(2) << static_cast<int>(remainingTime.asSeconds()) % 60 << std::flush;
 
-        //sterowanie 1
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && player1.getPosition().x > gridX) {
-            player1.move(-1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && player1.getPosition().x < gridX + gridWidth - player1.getSize().x) {
-            player1.move(1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && player1.getPosition().y > gridY) {
-            player1.move(0.0f, -1.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && player1.getPosition().y < gridY + gridHeight - player1.getSize().y) {
-            player1.move(0.0f, 1.0f);
-        }
 
-        //sterowanie 2
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player2.getPosition().x > gridX) {
-            player2.move(-1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player2.getPosition().x < gridX + gridWidth - player2.getSize().x) {
-            player2.move(1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player2.getPosition().y > gridY) {
-            player2.move(0.0f, -1.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player2.getPosition().y < gridY + gridHeight - player2.getSize().y) {
-            player2.move(0.0f, 1.0f);
-        }
+        if (!timeExpired) { //jezeli czas sie nie skonczyl
+            //sterowanie 1
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && player1.getPosition().x > gridX) {
+                player1.move(-1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && player1.getPosition().x < gridX + gridWidth - player1.getSize().x) {
+                player1.move(1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && player1.getPosition().y > gridY) {
+                player1.move(0.0f, -1.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && player1.getPosition().y < gridY + gridHeight - player1.getSize().y) {
+                player1.move(0.0f, 1.0f);
+            }
 
-        //sterowanie 3
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && player3.getPosition().x > gridX) {
-            player3.move(-1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3) && player3.getPosition().x < gridX + gridWidth - player3.getSize().x) {
-            player3.move(1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2) && player3.getPosition().y > gridY) {
-            player3.move(0.0f, -1.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4) && player3.getPosition().y < gridY + gridHeight - player3.getSize().y) {
-            player3.move(0.0f, 1.0f);
-        }
+            //sterowanie 2
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player2.getPosition().x > gridX) {
+                player2.move(-1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player2.getPosition().x < gridX + gridWidth - player2.getSize().x) {
+                player2.move(1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player2.getPosition().y > gridY) {
+                player2.move(0.0f, -1.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player2.getPosition().y < gridY + gridHeight - player2.getSize().y) {
+                player2.move(0.0f, 1.0f);
+            }
 
-        //sterowanie 4
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::J) && player4.getPosition().x > gridX) {
-            player4.move(-1.0f, 0.0f);
+            //sterowanie 3
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) && player3.getPosition().x > gridX) {
+                player3.move(-1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3) && player3.getPosition().x < gridX + gridWidth - player3.getSize().x) {
+                player3.move(1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2) && player3.getPosition().y > gridY) {
+                player3.move(0.0f, -1.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4) && player3.getPosition().y < gridY + gridHeight - player3.getSize().y) {
+                player3.move(0.0f, 1.0f);
+            }
+
+            //sterowanie 4
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::J) && player4.getPosition().x > gridX) {
+                player4.move(-1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::L) && player4.getPosition().x < gridX + gridWidth - player4.getSize().x) {
+                player4.move(1.0f, 0.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::I) && player4.getPosition().y > gridY) {
+                player4.move(0.0f, -1.0f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::K) && player4.getPosition().y < gridY + gridHeight - player4.getSize().y) {
+                player4.move(0.0f, 1.0f);
+            }
+
+            //oznacz odwiedzone pole
+            int player1GridX = (player1.getPosition().x - gridX) / gridSize;
+            int player1GridY = (player1.getPosition().y - gridY) / gridSize;
+
+            int player2GridX = (player2.getPosition().x - gridX) / gridSize;
+            int player2GridY = (player2.getPosition().y - gridY) / gridSize;
+
+            int player3GridX = (player3.getPosition().x - gridX) / gridSize;
+            int player3GridY = (player3.getPosition().y - gridY) / gridSize;
+
+            int player4GridX = (player4.getPosition().x - gridX) / gridSize;
+            int player4GridY = (player4.getPosition().y - gridY) / gridSize;
+
+            //ustalenie koloru na visited
+            if (player1GridX >= 0 && player1GridX < visited.size() && player1GridY >= 0 && player1GridY < visited[0].size()) {
+                visited[player1GridX][player1GridY] = player1Color;
+            }
+
+            if (player2GridX >= 0 && player2GridX < visited.size() && player2GridY >= 0 && player2GridY < visited[0].size()) {
+                visited[player2GridX][player2GridY] = player2Color;
+            }
+
+            if (player3GridX >= 0 && player3GridX < visited.size() && player3GridY >= 0 && player3GridY < visited[0].size()) {
+                visited[player3GridX][player3GridY] = player3Color;
+            }
+
+            if (player4GridX >= 0 && player4GridX < visited.size() && player4GridY >= 0 && player4GridY < visited[0].size()) {
+                visited[player4GridX][player4GridY] = player4Color;
+            }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L) && player4.getPosition().x < gridX + gridWidth - player4.getSize().x) {
-            player4.move(1.0f, 0.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::I) && player4.getPosition().y > gridY) {
-            player4.move(0.0f, -1.0f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::K) && player4.getPosition().y < gridY + gridHeight - player4.getSize().y) {
-            player4.move(0.0f, 1.0f);
-        }
-
-        //oznacz odwiedzone pole
-        int player1GridX = (player1.getPosition().x - gridX) / gridSize;
-        int player1GridY = (player1.getPosition().y - gridY) / gridSize;
-
-        int player2GridX = (player2.getPosition().x - gridX) / gridSize;
-        int player2GridY = (player2.getPosition().y - gridY) / gridSize;
-
-        int player3GridX = (player3.getPosition().x - gridX) / gridSize;
-        int player3GridY = (player3.getPosition().y - gridY) / gridSize;
-
-        int player4GridX = (player4.getPosition().x - gridX) / gridSize;
-        int player4GridY = (player4.getPosition().y - gridY) / gridSize;
-
-        //ustalenie koloru na visited
-        if (player1GridX >= 0 && player1GridX < visited.size() && player1GridY >= 0 && player1GridY < visited[0].size()) {
-            visited[player1GridX][player1GridY] = player1Color;
-        }
-
-        if (player2GridX >= 0 && player2GridX < visited.size() && player2GridY >= 0 && player2GridY < visited[0].size()) {
-            visited[player2GridX][player2GridY] = player2Color;
-        }
-
-        if (player3GridX >= 0 && player3GridX < visited.size() && player3GridY >= 0 && player3GridY < visited[0].size()) {
-            visited[player3GridX][player3GridY] = player3Color;
-        }
-
-        if (player4GridX >= 0 && player4GridX < visited.size() && player4GridY >= 0 && player4GridY < visited[0].size()) {
-            visited[player4GridX][player4GridY] = player4Color;
-        }
-
         window.clear();
 
         //rysowanko 
@@ -190,16 +193,13 @@ int main() {
         window.draw(player3);
         window.draw(player4);
     
-
         //okno
         window.display();
 
-        //czy koniec czasu
-        if (remainingTime <= sf::Time::Zero) {
-            break;
+        if (timeExpired) {
+            cout << "\nKONIEC CZASU\n";
         }
     }
-    std::cout << "\nKONIEC CZASU!\n";
 
     return 0;
 }
